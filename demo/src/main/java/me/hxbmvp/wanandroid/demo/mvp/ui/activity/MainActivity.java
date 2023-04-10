36b57347c8df969b41dbd12d9c4154d8bf84e13b
HeXingBo <386985454@qq.com>
2023-04-10 16:04:24 +0800

    添加主界面底部菜单和侧边栏菜单

--- demo/src/main/java/me/hxbmvp/wanandroid/demo/mvp/ui/activity/MainActivity.java
+++ demo/src/main/java/me/hxbmvp/wanandroid/demo/mvp/ui/activity/MainActivity.java
@@ -2,23 +2,29 @@ package me.hxbmvp.wanandroid.demo.mvp.ui.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.FrameLayout;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationView;
 import com.jess.arms.base.BaseActivity;
 import com.jess.arms.di.component.AppComponent;
 import com.jess.arms.utils.ArmsUtils;
 
+import butterknife.BindView;
 import me.hxbmvp.wanandroid.demo.di.component.DaggerMainComponent;
 import me.hxbmvp.wanandroid.demo.mvp.contract.MainContract;
 import me.hxbmvp.wanandroid.demo.mvp.presenter.MainPresenter;
 
 import me.hxbmvp.wanandroid.demo.R;
 
-
 import static com.jess.arms.utils.Preconditions.checkNotNull;
-
 
 /**
  * @作者：hexingbo
@@ -26,6 +32,21 @@ import static com.jess.arms.utils.Preconditions.checkNotNull;
  * @描述：
  */
 public class MainActivity extends BaseActivity<MainPresenter> implements MainContract.View {
+
+    @BindView(R.id.drawer_layout)
+    DrawerLayout mDrawerLayout;
+    @BindView(R.id.toolbar)
+    Toolbar mToolbar;
+    @BindView(R.id.toolbar_title)
+    TextView mTitleTv;
+    @BindView(R.id.main_floating_action_btn)
+    FloatingActionButton mFloatingActionButton;
+    @BindView(R.id.bottom_navigation_view)
+    BottomNavigationView mBottomNavigationView;
+    @BindView(R.id.nav_view)
+    NavigationView mNavigationView;
+    @BindView(R.id.fragment_group)
+    FrameLayout mFrameGroup;
 
     @Override
     public void setupActivityComponent(@NonNull AppComponent appComponent) {
@@ -44,7 +65,7 @@ public class MainActivity extends BaseActivity<MainPresenter> implements MainCon
 
     @Override
     public void initData(@Nullable Bundle savedInstanceState) {
-
+        initBottomNavigationView();
     }
 
     @Override
@@ -72,5 +93,35 @@ public class MainActivity extends BaseActivity<MainPresenter> implements MainCon
     @Override
     public void killMyself() {
         finish();
+    }
+
+    private void initBottomNavigationView() {
+        mBottomNavigationView.setOnNavigationItemSelectedListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.tab_main_pager:
+//                    loadPager(getString(R.string.home_pager), 0,
+//                            mMainPagerFragment, Constants.TYPE_MAIN_PAGER);
+                    break;
+                case R.id.tab_knowledge_hierarchy:
+//                    loadPager(getString(R.string.knowledge_hierarchy), 1,
+//                            mKnowledgeHierarchyFragment, Constants.TYPE_KNOWLEDGE);
+                    break;
+                case R.id.tab_wx_article:
+//                    loadPager(getString(R.string.wx_article), 2,
+//                            mWxArticleFragment, Constants.TYPE_WX_ARTICLE);
+                    break;
+                case R.id.tab_navigation:
+//                    loadPager(getString(R.string.navigation), 3,
+//                            mNavigationFragment, Constants.TYPE_NAVIGATION);
+                    break;
+                case R.id.tab_project:
+//                    loadPager(getString(R.string.project), 4,
+//                            mProjectFragment, Constants.TYPE_PROJECT);
+                    break;
+                default:
+                    break;
+            }
+            return true;
+        });
     }
 }
